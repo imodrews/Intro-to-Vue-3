73005c8b7bfe1d244b2a68f561a1b1d0d72e912a
Imo <imoleadrews@gmail.com>
2021-01-05 11:50:17 +0100

    Changes to first lesson

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            description: 'Nice warm socks to keep you cozy this winter!'
+        }
+    }
+})
+// Did this using the ES6 shorthand 
